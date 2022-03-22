5e323343ef3ac7697da150353c888c31f9d95db3
Tim Quimbo <tquimbo1@gmail.com>
2022-03-21 22:43:05 -0400

    Changed template

---
+++ my-app/src/components/about/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import classes from './About.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+
+class About extends Component {
+    render() {
+        return (
+            <div className={classes.box} id="about">
+                <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+                    <span className={classes.head}>ABOUT ME</span>
+                    <h2 className={classes.heading}>Who Am I?</h2>
+                    <div className={classes.About}>
+                        <p> My name is <b>TIM QUIMBO</b> </p>
+                        <p className={classes.br}>
+                        </p>
+                    </div>
+                </ScrollAnimation>
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ my-app/src/components/sidebar/Sidebar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+import { HashLink as Link } from 'react-router-hash-link';
+import { FiExternalLink } from 'react-icons/fi';
+import logo from '../images/tim.jpeg'
+
+class Sidebar extends Component {
+    render() {
+
+        return (
+            <div className="sidebar">
+                <div style={{ color: 'black', fontWeight: 'bold' }} className="tagtop"> # programmer_life </div>
+                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # hello_world </div>
+                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # coding </div>
+                <h1><Link smooth to="/#start" className="h1_links">Tim Quimbo</Link></h1>
+
+                <img src={logo} />
+                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> tquimbo1@gmail.com </p>
+
+                <ul className="sidebar-nav">
+                    <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
+                    {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
+                </ul>
+
+                <div className="flip-card-back">
+                    <ul className="sidebar-nav">
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/tquimbo" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/tim-quimbo-a3b273a7/" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
+
+                        <li className="sidebar-nav-icons"> <a href="mailto:tquimbo1@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
+
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Sidebar
